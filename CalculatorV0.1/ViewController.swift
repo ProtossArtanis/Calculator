3c8fb69eec91a9fb61fe507539cadc2d96a95705
ProtossArtanis <steinsgate@mac24.local>
2016-03-24 11:57:10 +0800

    结构体
    结构体

--- CalculatorV0.1/ViewController.swift
+++ CalculatorV0.1/ViewController.swift
@@ -19,6 +19,39 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func Button_1(sender: UIButton) {
+    }
+    @IBAction func Button_2(sender: UIButton) {
+    }
+    @IBAction func Button_3(sender: UIButton) {
+    }
+    @IBAction func Button_4(sender: UIButton) {
+    }
+    @IBAction func Button_5(sender: UIButton) {
+    }
+    @IBAction func Button_6(sender: UIButton) {
+    }
+    @IBAction func Button_7(sender: UIButton) {
+    }
+    @IBAction func Button_8(sender: UIButton) {
+    }
+    @IBAction func Button_9(sender: UIButton) {
+    }
+    @IBAction func Button_0(sender: UIButton) {
+    }
+    @IBAction func Button_point(sender: UIButton) {
+    }
+    @IBAction func Button_add(sender: UIButton) {
+    }
+    @IBAction func Button_subtraction(sender: UIButton) {
+    }
+    @IBAction func Button_multiplication(sender: UIButton) {
+    }
+    @IBAction func Button_division(sender: AnyObject) {
+    }
+    @IBAction func Button_equal(sender: UIButton) {
+    }
+    @IBOutlet weak var OutPutResult: UITextField!
 
 
 }
---
+++ CalculatorV0.1/calcula.swift
@@ -0,0 +1,13 @@
+//
+//  calcula.swift
+//  CalculatorV0.1
+//
+//  Created by SteinsGate on 16/3/24.
+//  Copyright © 2016年 SteinsGate. All rights reserved.
+//
+
+import Foundation
+
+struct calcula {
+    private var NowCalcula:
+}
